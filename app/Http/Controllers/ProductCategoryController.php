618870297a534febc6d2a8bdb452262052c1ecf0
ropehapi <ropehapi@gmail.com>
2022-05-04 23:05:07 -0300

    Products categories management done

---
+++ app/Http/Controllers/ProductCategoryController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductCategory;
+use Illuminate\Http\Request;
+
+class ProductCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ProductCategory::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $productCategory = new ProductCategory();
+        $productCategory->name = $request->name;
+        $productCategory->save();
+
+        return $productCategory;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProductCategory  $productCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductCategory $productCategory)
+    {
+        return $productCategory;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProductCategory  $productCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductCategory $productCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ProductCategory  $productCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProductCategory $productCategory)
+    {
+        $productCategory->name = $request->name;
+        $productCategory->update();
+
+        return $productCategory;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProductCategory  $productCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductCategory $productCategory)
+    {
+        $productCategory->delete();
+        return ProductCategory::all();
+    }
+}
